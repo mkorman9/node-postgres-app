b07ebb1f693f6ff7b08a42b964b9d225307e443e
Michal Korman <michal.korman@icloud.com>
2023-12-08 13:53:57 +0100

    prevent process from hanging after finishing jest tests

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,13 +1,18 @@
 import knex from 'knex';
 import config from '../config';
 
-export default knex({
+const db = knex({
   client: 'pg',
   connection: {
     connectionString: config.DB_URL,
   },
   pool: {
     min: 0,
-    max: 5
+    max: 5,
+
+    // to prevent process from hanging after jest tests are finished
+    idleTimeoutMillis: (process.env.NODE_ENV === 'test') ? 500 : undefined
   }
 });
+
+export default db;

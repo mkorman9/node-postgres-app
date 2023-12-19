6878a99d65ab13e3719f07835fb01ffba125daf2
Michal Korman <michal.korman@icloud.com>
2023-12-19 22:01:36 +0100

    fix Dockerfile

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import {appendErrorHandlers, createApp} from './http/app_template';
 import todoItemsApi from './todo_items_api';
+import {Request, Response} from 'express';
 
 const app = createApp();
 
 //app.use('/', express.static('./public'));
+app.get('/', async (req: Request, res: Response) => {
+  res.json({
+    content: 'hello world'
+  });
+});
+
 app.use(todoItemsApi);
 
 export default appendErrorHandlers(app);
